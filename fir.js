83491418f8efdec81765528310a31f935eded38b
Pranav-Ajay <pranavajay005@gmail.com>
2026-02-19 07:29:33 +0530

    Update fir.js
    Signed-off-by: Pranav-Ajay <pranavajay005@gmail.com>

--- fir.js
+++ fir.js
@@ -1,3 +1,5 @@
+src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
+
 function getCurrentDateTime() {
     const now = new Date();
     const day = String(now.getDate()).padStart(2, '0');
